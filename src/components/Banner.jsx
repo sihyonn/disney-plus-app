701be3eb6487c5aedc228e3b4689e4cf33f1a9ae
sihyonn <sihyeon982@naver.com>
2023-09-01 01:19:12 +0900

    feat: 비디오 배너 생성

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,39 @@ import axios from "../api/axios";
 import requests from "../api/requests";
 import React, { useEffect, useState } from "react";
 
+import { styled } from "styled-components";
 import "../style/Banner.css";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -31,7 +63,22 @@ const Banner = () => {
   };
 
   if (isClicked) {
-    return <div>clicked</div>;
+    return (
+      <>
+        <Container>
+          <HomeContainer>
+            <Iframe
+              src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+              width="640"
+              height="360"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+            ></Iframe>
+          </HomeContainer>
+        </Container>
+        <button onClick={() => setIsClicked(false)}>X</button>
+      </>
+    );
   } else {
     return (
       <header
