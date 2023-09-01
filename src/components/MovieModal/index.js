dae1f9cd865629fb733a8a53585ae735f3972b89
sihyonn <sihyeon982@naver.com>
2023-09-01 17:17:22 +0900

    style: MovieModal CSS 적용

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../MovieModal/MovieModal.css";
 
 const MovieModal = ({
   backdrop_path,
@@ -13,25 +14,27 @@ const MovieModal = ({
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <span onClick={() => setModalOpen(false)} className="modal-close">
-          X
-        </span>
+        <div className="modal">
+          <span onClick={() => setModalOpen(false)} className="modal-close">
+            X
+          </span>
 
-        <img
-          className="modal__poster-img"
-          src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-          alt="modal-img"
-        />
+          <img
+            className="modal__poster-img"
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+            alt="modal-img"
+          />
 
-        <div className="modal__content">
-          <p className="modal__details">
-            <span className="modal__user_perc">100% for you</span>{" "}
-            {release_date ? release_date : first_air_date}
-          </p>
+          <div className="modal__content">
+            <p className="modal__details">
+              <span className="modal__user_perc">100% for you</span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
 
-          <h2 className="modal__title"> {title ? title : name}</h2>
-          <p className="modal__overview">평점: {vote_average}</p>
-          <p className="modal__overview">{overview}</p>
+            <h2 className="modal__title"> {title ? title : name}</h2>
+            <p className="modal__overview">평점: {vote_average}</p>
+            <p className="modal__overview">{overview}</p>
+          </div>
         </div>
       </div>
     </div>
