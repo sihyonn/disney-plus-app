83725c38626b39eb10a3f1f44d362d1f5e6141a9
sihyonn <sihyeon982@naver.com>
2023-09-01 16:57:08 +0900

    feat: 모달 초기생성

---
+++ src/components/MovieModal/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MovieModal = ({
+  backdrop_path,
+  title,
+  overview,
+  name,
+  release_date,
+  first_air_date,
+  vote_average,
+  setModalOpen,
+}) => {
+  return <div>MovieModal</div>;
+};
+
+export default MovieModal;
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "../api/axios";
 import "../style/Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const res = await axios.get(fetchUrl);
@@ -15,7 +18,10 @@ const Row = ({ title, id, fetchUrl }) => {
     fetchMovieData();
   }, [fetchMovieData]);
 
-  const handleClick = () => {};
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
   return (
     <div>
@@ -37,6 +43,7 @@ const Row = ({ title, id, fetchUrl }) => {
               key={movie.id}
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />;
           })}
         </div>
@@ -51,6 +58,9 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
