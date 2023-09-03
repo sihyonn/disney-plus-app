9a7f73bcec9670dc0311070e1ae5cb291a3e3d15
sihyonn <sihyeon982@naver.com>
2023-09-03 18:41:42 +0900

    feat: swiper로 슬라이드로 터치슬라이드 구현

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,54 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "../api/axios";
-import "../style/Row.css";
 import MovieModal from "./MovieModal";
+import "../style/Row.css";
+import { styled } from "styled-components";
+
+// import swiper & swiper style
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+const Content = styled.div``;
+
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+    rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index: 1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
@@ -11,6 +58,7 @@ const Row = ({ title, id, fetchUrl }) => {
   const fetchMovieData = useCallback(async () => {
     const res = await axios.get(fetchUrl);
     console.log("res", res);
+    console.log("movies는", movies);
     setMovies(res.data.results);
   }, [fetchUrl]);
 
@@ -24,49 +72,53 @@ const Row = ({ title, id, fetchUrl }) => {
   };
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
-        <div>
-          {movies.map((movie) => {
-            console.log(
-              `Image URL for ${movie.name}: https://image.tmdb.org/t/p/original${movie.backdrop_path}`
-            );
-            return (
-              <img
-                key={movie.id}
-                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                alt={movie.name}
-                onClick={() => handleClick(movie)}
-              />
-            );
-          })}
-        </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+      <Swiper
+        // swiper 모듈 설치
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // 데이터 끝나도 넘어가주게
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} // 동그라미들
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, //한번에 보이는 슬라이드 개수
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5, //한번에 보이는 슬라이드 개수
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4, //한번에 보이는 슬라이드 개수
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3, //한번에 보이는 슬라이드 개수
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        <Content id={id}>
+          {movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <Wrap>
+                <img
+                  key={movie.id}
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </Wrap>
+            </SwiperSlide>
+          ))}
+        </Content>
+      </Swiper>
+
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
-    </div>
+    </Container>
   );
 };
 
