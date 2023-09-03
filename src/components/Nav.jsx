e46616715a6c7a761c13458b38434f9bf370bb5d
sihyonn <sihyeon982@naver.com>
2023-09-03 20:52:32 +0900

    style: 로그아웃 스타일링

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -63,11 +63,43 @@ const Input = styled.input`
   border: none;
 `;
 
-const SignOut = styled.div``;
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100%;
+  opacity: 0;
+`;
 
-const UserImg = styled.div``;
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
 
-const DropDown = styled.div``;
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+`;
 
 const Nav = () => {
   const [show, setShow] = useState(false);
