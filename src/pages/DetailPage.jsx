34d3999a5984d2aecc00d31f66bb78f2c4f792e5
sihyonn <sihyeon982@naver.com>
2023-09-03 16:41:21 +0900

    feat: DetailPage 구현

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const [movie, setMovie] = useState({});
+
+  // let movieId = useParams().movieId와 동일
+  let { movieId } = useParams();
+  console.log(movieId);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      console.log(response);
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) return null;
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="movie_img"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
