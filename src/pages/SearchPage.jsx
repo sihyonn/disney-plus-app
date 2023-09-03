bd25eef393f5663dcad15b128035ab7144a56ba3
sihyonn <sihyeon982@naver.com>
2023-09-03 16:25:50 +0900

    feat: searchTerm => debouncedSearchTerm으로 변환

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "../style/SearchPage.css";
+import { useDebounce } from "../hooks/useDebounce";
 
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -13,12 +14,13 @@ const SearchPage = () => {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
